fe5f38c861fbbd935fa91117495f499233ff6053
3str-webdev <3str.webdev@gmail.com>
2024-03-16 10:25:11 +0300

    update: change donate button size

--- src/widgets/donate/ui/donate.svelte
+++ src/widgets/donate/ui/donate.svelte
@@ -10,14 +10,16 @@
 <section class="min-h-14">
   <button
     class="{twJoin(
-      'fixed bottom-4 left-4 overflow-hidden w-11',
+      'fixed bottom-4 left-4 overflow-hidden w-8 h-8',
       'flex items-center gap-2',
-      'p-2 bg-[#aa6148] rounded-full transition-all',
-      'hover:w-[210px]',
+      'px-1 bg-[#aa6148] rounded-full transition-all',
+      'hover:w-[180px]'
     )}"
     on:click="{handleClick}"
   >
-    <CoffeeIcon class="min-w-7 min-h-7" />
-    <p class="text-white pr-2 whitespace-nowrap">Поддержать проект</p>
+    <CoffeeIcon class="min-w-6 min-h-6" />
+    <p class="text-white pr-[6px] whitespace-nowrap text-sm">
+      Поддержать проект
+    </p>
   </button>
 </section>
