7a35b2d9497cd0a05afc5e70bb18c70a525ef439
3str-webdev <3str.webdev@gmail.com>
2024-03-24 23:04:17 +0300

    update: change notice styles

--- src/entities/notice/ui/notice-view.svelte
+++ src/entities/notice/ui/notice-view.svelte
@@ -9,7 +9,7 @@
     transition:fly="{{ duration: 200, y: 10 }}"
     class="{twJoin('fixed w-full bottom-5 z-50', 'flex justify-center')}"
   >
-    <span class="px-3 py-1 text-sm rounded-md bg-slate-200 select-none">
+    <span class="px-4 py-2 text-sm bg-popover rounded-md shadow-lg select-none">
       {$noticeStore.message}
     </span>
   </div>
