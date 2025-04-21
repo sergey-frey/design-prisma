b725174dd8fece51092bec26469a8d00611be315
sergey-frey <sergey.frey.work@gmail.com>
2025-04-21 12:03:32 +0300

    feat: add context menu hint for code fields

--- src/features/code-field/ui/code-field.svelte
+++ src/features/code-field/ui/code-field.svelte
@@ -1,72 +1,84 @@
 <script lang="ts">
-  import { settingsStore } from "@/entities/settings";
-  import { content } from "@/shared/content";
-  import type { NodeCSS } from "@/shared/types";
-  import { formatStyles } from "@/shared/utils";
-  import { format } from "prettier";
-  import htmlParser from "prettier/parser-html";
-  import { Highlight } from "svelte-highlight";
-  import { plaintext, xml } from "svelte-highlight/languages";
-  import theme from "svelte-highlight/styles/atelier-cave-light";
-  import CodeHighlight from "./code-highlight.svelte";
-  import CopyButton from "./copy-button.svelte";
+	import { settingsStore } from "@/entities/settings";
+	import { content } from "@/shared/content";
+	import type { NodeCSS } from "@/shared/types";
+	import { RmbIcon } from "@/shared/ui/icons";
+	import { formatStyles } from "@/shared/utils";
+	import clsx from "clsx";
+	import { format } from "prettier";
+	import htmlParser from "prettier/parser-html";
+	import { Highlight } from "svelte-highlight";
+	import { plaintext, xml } from "svelte-highlight/languages";
+	import theme from "svelte-highlight/styles/atelier-cave-light";
+	import type { HTMLAttributes } from "svelte/elements";
+	import { applyModifications } from "../model/modification-node-css";
+	import "../styles/index.css";
+	import CodeHighlight from "./code-highlight.svelte";
+	import CopyButton from "./copy-button.svelte";
 
-  import { applyModifications } from "../model/modification-node-css";
-  import "../styles/index.css";
+	type Props = HTMLAttributes<HTMLDivElement> & {
+		code: NodeCSS | string;
+		isSVG?: boolean;
+		rmbNotice?: boolean;
+	};
 
-  export let code: NodeCSS | string;
-  export let isSVG: boolean = false;
-  export let rmbNotice: boolean = false;
+	let { isSVG, rmbNotice, code }: Props = $props();
 
-  const getHighlightProps = async (code: string | NodeCSS) => {
-    if (isSVG && typeof code === "string") {
-      return {
-        language: xml,
-        code: await format(code, {
-          parser: "html",
-          plugins: [htmlParser],
-        }),
-      };
-    }
+	const getHighlightProps = async (code: string | NodeCSS) => {
+		if (isSVG && typeof code === "string") {
+			return {
+				language: xml,
+				code: await format(code, {
+					parser: "html",
+					plugins: [htmlParser],
+				}),
+			};
+		}
 
-    return {
-      language: plaintext,
-      code: code as string,
-    };
-  };
+		return {
+			language: plaintext,
+			code: code as string,
+		};
+	};
 
-  $: highlightProps = getHighlightProps(code);
+	const highlightProps = $derived(getHighlightProps(code));
 </script>
 
 <svelte:head>
-  {@html theme}
+	{@html theme}
 </svelte:head>
 
 {#if code}
-  <div>
-    <div class="flex items-center justify-between">
-      <slot name="title" />
-      {#if rmbNotice}
-        <span class="text-sm font-semibold pr-1">
-          {content[$settingsStore.lang].codeField.rmb}
-        </span>
-      {/if}
-    </div>
-    <div class="relative">
-      {#if typeof code === "string"}
-        {#await highlightProps then { language, code }}
-          <CopyButton {code} class="absolute top-2 right-2" />
-          <Highlight class="text-sm" {language} {code} />
-        {/await}
-      {:else}
-        {@const codeWithModifications = applyModifications(
-          code,
-          $settingsStore
-        )}
-        {@const codeString = formatStyles(codeWithModifications)}
-        <CopyButton code="{codeString}" class="absolute top-2 right-2" />
-        <CodeHighlight code="{codeWithModifications}" />
-      {/if}
-    </div>
-  </div>
+	<div>
+		<div class="flex items-center justify-between">
+			<slot name="title" />
+		</div>
+		<div class="relative">
+			{#if typeof code === "string"}
+				{#await highlightProps then { language, code }}
+					<CopyButton {code} class="absolute top-2 right-2" />
+					<Highlight class="text-sm" {language} {code} />
+				{/await}
+			{:else}
+				{@const codeWithModifications = applyModifications(
+					code,
+					$settingsStore
+				)}
+				{@const codeString = formatStyles(codeWithModifications)}
+				<CopyButton code={codeString} class="absolute top-2 right-2" />
+				<CodeHighlight
+					code={codeWithModifications}
+					className={clsx({ ["rounded-b-none"]: rmbNotice })}
+				/>
+			{/if}
+		</div>
+		{#if rmbNotice}
+			<span
+				class="text-xs px-2 flex items-center gap-1 bg-slate-200 rounded-b-sm py-1"
+			>
+				{content[$settingsStore.lang].codeField.rmb}
+				<RmbIcon class="w-3 h-3" />
+			</span>
+		{/if}
+	</div>
 {/if}
--- src/features/code-field/ui/code-highlight.svelte
+++ src/features/code-field/ui/code-highlight.svelte
@@ -1,23 +1,25 @@
 <script lang="ts">
-  import * as ContextMenu from "$lib/components/ui/context-menu";
-  import type { NodeCSS } from "@/shared/types";
-  import { twJoin } from "tailwind-merge";
-  import CodeRow from "./code-row.svelte";
+	import * as ContextMenu from "$lib/components/ui/context-menu";
+	import type { NodeCSS } from "@/shared/types";
+	import { twJoin } from "tailwind-merge";
+	import CodeRow from "./code-row.svelte";
+	import clsx from "clsx";
 
-  export let code: NodeCSS;
+	export let code: NodeCSS;
+	export let className: string = "";
 
-  $: rules = Object.keys(code) as Array<keyof NodeCSS>;
+	$: rules = Object.keys(code) as Array<keyof NodeCSS>;
 </script>
 
-<div class="rounded-sm overflow-hidden">
-  <div class="flex overflow-auto max-h-[300px] bg-muted">
-    <div class="{twJoin('grow text-sm py-1 px-1', 'whitespace-nowrap')}">
-      {#each rules as rule}
-        {#if code[rule]}
-          {@const value = code[rule]}
-          <CodeRow {rule} {value} />
-        {/if}
-      {/each}
-    </div>
-  </div>
+<div class={clsx("rounded-sm overflow-hidden", className)}>
+	<div class="flex overflow-auto max-h-[300px] bg-muted">
+		<div class={twJoin("grow text-sm py-1 px-1", "whitespace-nowrap")}>
+			{#each rules as rule}
+				{#if code[rule]}
+					{@const value = code[rule]}
+					<CodeRow {rule} {value} />
+				{/if}
+			{/each}
+		</div>
+	</div>
 </div>
